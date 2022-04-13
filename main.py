dc91077fb0d31d1a250182e2e863e878ee636129
filipgaldyn <filip.galdyn.fg@gmail.com>
2022-04-13 22:46:59 +0200

    repair bugs connected with adding rows to datafile

--- main.py
+++ main.py
@@ -16,12 +16,22 @@ def filtration(df):
     stats = [datetime.datetime.now().strftime("%Y-%m-%d"), x_filtr.shape[0], round(x_filtr.loc[:,'price'].mean(),2), std]
     return stats, x_filtr
 
+def filtration_iqr(df):
+    q1 = df.loc[:,'price'].quantile(0.25)
+    q3 = df.loc[:,'price'].quantile(0.75)
+    iqr = q3 - q1
+    low_boundary = (q1 - 1.5 * iqr)
+    upp_boundary = (q3 + 1.5 * iqr)
+    x_filtr = df[(df.loc[:,'price']>low_boundary) & (df.loc[:,'price']<upp_boundary)].reset_index(drop=True)
+    stats = [datetime.datetime.now().strftime("%Y-%m-%d"), x_filtr.shape[0], round(x_filtr.loc[:,'price'].mean(),2), iqr]
+    return stats, x_filtr
+
 def main():
     dirname = os.path.dirname(os.path.abspath("__file__"))
     dirname = os.path.join(dirname, "daily_databases")
     list_of_file = os.listdir(dirname)
     for pick in list_of_file:
-        all = pd.read_csv(os.path.join(dirname, pick))
+        all = pd.read_csv(os.path.join(dirname, pick), index_col='index')
         pick = pick.split('.')
         city = pick[0]
         base_URL = f"https://www.olx.pl/nieruchomosci/mieszkania/wynajem/{city}/"
@@ -29,12 +39,13 @@ def main():
         bs = BeautifulSoup(page.content, "html.parser")
         num_of_pages = number_of_pages(page, bs)
         df = data_from_city(city, base_URL, num_of_pages)
-        stat, filtrated_data = filtration(df)
+        #stat, filtrated_data = filtration(df)
+        stat, filtrated_data = filtration_iqr(df)
         x = pd.DataFrame(stat).T
         x.columns = ['date','number','mean', 'std']
         all = pd.concat([all, x], ignore_index=True)
-        all.to_csv(f"{dirname}/{pick[0]}.csv")
-        
+        all.to_csv(f"{dirname}/{pick[0]}.csv", index_label='index')
+        print(x, pick[0])
     return df
 
 if __name__ == '__main__':
